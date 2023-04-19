9a53351e4fb9d9e401aa5962e132e1be7782bb23
AndrzejPuczko <puczko.andrzej@gmail.com>
2023-04-19 21:21:07 +0200

    Added labels of categories, genres of films

---
+++ src/ts/labels.ts
@@ -0,0 +1,21 @@
+export const genreLabels: {[key: number]: string} = {
+	28: 'Akcja',
+	12: 'Przygodowy',
+	16: 'Animacja',
+	35: 'Komedia',
+	80: 'Kryminał',
+    99: 'Dokumentalny',
+    18: 'Dramat',
+    10751: 'Familijny',
+    14: 'Fantasy',
+    36: 'Historyczny',
+    27: 'Horror',
+    10402: 'Muzyczny',
+    9648: 'Tajemnica',
+    10749: 'Romans',
+    878: 'Sci-Fi',
+    10770: 'film TV',
+    53: 'Thriller',
+    10752: 'Wojenny',
+    37: 'Western'
+}
